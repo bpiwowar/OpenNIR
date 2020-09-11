2560039a2cd48b2e850a65e212ff6ef341c1017e
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-09-11 14:39:56 +0200

    Setup

--- setup.py
+++ setup.py
@@ -14,26 +14,22 @@ install_requires = (basepath / "requirements.txt").read_text()
 install_requires = re.sub(r"^(git\+https.*)egg=([_\w-]+)$", r"\2@\1", install_requires, 0, re.MULTILINE)
 
 setup(
-    name='OpenNIR_XPM',
+    name='OpenNIR-XPM',
     description='OpenNIR: A Complete Neural Ad-Hoc Ranking Pipeline (Experimaestro version)',
-    author="Sean MacAvaney (modified by Benjamin Piwowarski)",
+    author="Sean MacAvaney (adapted by Benjamin Piwowarski for experimaestro)",
     author_email="b@piwowarski.fr",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.1.0',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     packages=find_packages(),
-    url="http://opennir.net/",
+    url="https://github.com/bpiwowar/OpenNIR-xpm",
     project_urls={
-        "Documentation": "http://opennir.net/",
-        "Source": "https://github.com/bpiwowar/OpenNIR"
+        "Documentation": "https://github.com/bpiwowar/OpenNIR-xpm",
+        "Source": "https://github.com/bpiwowar/OpenNIR-xpm"
     },
     install_requires=install_requires,
     entry_points={
-        "console_scripts": [
-            "onir_init_dataset=onir.bin.init_dataset:main",
-            "onir_pipeline=onir.bin.pipeline:main",
-            "onir_eval=onir.bin.eval:main",
-        ],
     },
     python_requires='>=3.7',
 )
